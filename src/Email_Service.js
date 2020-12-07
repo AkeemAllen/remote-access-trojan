9739b85d3743ef347fdf124bc05be572fed8d3bf
Chad <chadoliver017@gmail.com>
2020-12-06 20:05:26 -0500

    email body update

--- src/Email_Service.js
+++ src/Email_Service.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: "", //we might have to create a temp gmail account or something to have a username and password
+        user: "johnbrowncsg@gmail.com", //we might have to create a temp gmail account or something to have a username and password
                     //here for it to work
-        pass: ""
+        pass: "Johnbrowncsg123"
     }
   });
   
@@ -17,14 +17,17 @@ exports.send = (email) => {
     var message =  {
         from: 'test@test.com',
         to: email,
-        subject: 'Test email subject',
-        html: '<h1>Test email header</h1><p>Test email body<b>',
-        attachments: [
-            { // Use a URL as an attachment
-              filename: 'your-testla.png',
-              path: 'src/assets/utechlogo.png'
-          }
-        ]
+        subject: 'SCIT Past Papers',
+        html: '<p>Greetings fellow SCIT Bulldogs! Finals are just around the corner, and we know how' +
+         'stressful prepartions may be. Attached is a link with a few past papers to help get you started as well as' +
+         'a grade calculator to help you determine your current grades as well as how much you will need to pass a course'+ 
+         ' https://drive.google.com/file/d/19v6Gcr-nm8ii4MeIFo6x6to0iYfE44Le/view?usp=sharing<p>',
+        // attachments: [
+        //     { // Use a URL as an attachment
+        //       filename: 'your-testla.png',
+        //       path: 'src/assets/utechlogo.png'
+        //   }
+        // ]
     };
 
     transporter.sendMail(message, function(err, info) {
